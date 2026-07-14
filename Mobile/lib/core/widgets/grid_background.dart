ebdf258b700d34c4dd9abe4874ff73ed4e7a7ee5
YASH PAL <2004yashpal@gmail.com>
2026-07-14 21:51:16 +0530

    Remove grid pattern from background globally

--- Mobile/lib/core/widgets/grid_background.dart
+++ Mobile/lib/core/widgets/grid_background.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import '../constants/app_colors.dart';
 
 class GridBackground extends StatelessWidget {
   final Widget child;
@@ -13,53 +12,6 @@ class GridBackground extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final isDark = theme.brightness == Brightness.dark;
-
-    // Faint drafting paper grid color
-    final gridColor = isDark
-        ? Colors.white.withValues(alpha: 0.015)
-        : AppColors.primary.withValues(alpha: 0.035);
-
-    return CustomPaint(
-      painter: _GridPainter(
-        gridColor: gridColor,
-        spacing: spacing,
-      ),
-      child: child,
-    );
-  }
-}
-
-class _GridPainter extends CustomPainter {
-  final Color gridColor;
-  final double spacing;
-
-  const _GridPainter({
-    required this.gridColor,
-    required this.spacing,
-  });
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint()
-      ..color = gridColor
-      ..strokeWidth = 1.0
-      ..style = PaintingStyle.stroke;
-
-    // Draw vertical grid lines
-    for (double x = 0; x < size.width; x += spacing) {
-      canvas.drawLine(Offset(x, 0), Offset(x, size.height), paint);
-    }
-
-    // Draw horizontal grid lines
-    for (double y = 0; y < size.height; y += spacing) {
-      canvas.drawLine(Offset(0, y), Offset(size.width, y), paint);
-    }
-  }
-
-  @override
-  bool shouldRepaint(covariant _GridPainter oldDelegate) {
-    return oldDelegate.gridColor != gridColor || oldDelegate.spacing != spacing;
+    return child;
   }
 }
